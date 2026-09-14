fa07ea97e72f7d96d431f8170d0a9d3dc39103b8
loisAboveLimits <lois@above-limits.com>
2026-09-14 11:13:25 +0000

    added classic editor and ssh plugins

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,4 @@
 <?php
-define( 'WP_CACHE', true ); // Added by WP Rocket
-
 /**
  * The base configuration for WordPress
  *
@@ -15,56 +13,29 @@ define( 'WP_CACHE', true ); // Added by WP Rocket
  * * Database table prefix
  * * ABSPATH
  *
- * This has been slightly modified (to read environment variables) for use in Docker.
- *
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
  *
  * @package WordPress
  */
 
-// IMPORTANT: this file needs to stay in-sync with https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
-// (it gets parsed by the upstream wizard in https://github.com/WordPress/WordPress/blob/f27cb65e1ef25d11b535695a660e7282b98eb742/wp-admin/setup-config.php#L356-L392)
-
-// a helper function to lookup "env_FILE", "env", then fallback
-if (!function_exists('getenv_docker')) {
-	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
-	function getenv_docker($env, $default) {
-		if ($fileEnv = getenv($env . '_FILE')) {
-			return rtrim(file_get_contents($fileEnv), "\r\n");
-		}
-		else if (($val = getenv($env)) !== false) {
-			return $val;
-		}
-		else {
-			return $default;
-		}
-	}
-}
-
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
+define( 'DB_NAME', 'jycdb' );
 
 /** Database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
+define( 'DB_USER', 'adminuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
-
-/**
- * Docker image fallback values above are sourced from the official WordPress installation wizard:
- * https://github.com/WordPress/WordPress/blob/1356f6537220ffdc32b9dad2a6cdbe2d010b7a88/wp-admin/setup-config.php#L224-L238
- * (However, using "example username" and "example password" in your database is strongly discouraged.  Please use strong, random credentials!)
- */
+define( 'DB_PASSWORD', 'admin_003' );
 
 /** Database hostname */
-define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8mb4') );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication unique keys and salts.
@@ -77,15 +48,14 @@ define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         'da38907124aea00a8f442a35c7282fe5b63ca2a8') );
-define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  '23fae3ca25af808ebeaf17445be3d23378c36f34') );
-define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    '8ad8c4b70e085fe8f63fa92f88ad4d498f6e72d9') );
-define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        'e88c87c10853347ad0b33561967d3cf7ed37b34c') );
-define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        'cbe7c4b921bad14ce6fe358af304a495e4e23a89') );
-define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', '2af0636688ad20db526cfbc85c09f0eed47152cb') );
-define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   '3624432109f94836bb5b8e907ad45443fa0a1ac6') );
-define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       '927b15df11679910abe65deef2a85fc4757fd6c5') );
-// (See also https://wordpress.stackexchange.com/a/152905/199287)
+define( 'AUTH_KEY',         '(~fG55vU}o^<)aQq.V#Rc~W70hP>^XaqMALjg(YbYKA6RE<_<5wiQaA{y=o*UB5x' );
+define( 'SECURE_AUTH_KEY',  'QNo5YRg1&&:R~h*ylnV*CK|O]$,zu`lGqFiG+gxp!XEvUK&:]=6Oz<HM,~fe1kMI' );
+define( 'LOGGED_IN_KEY',    ';cc/WN$W5?@Q$&KZD4s oepW-Z$sa0D ?pF:Ej~9IJ*:ObmeDGXVd~_cQf8;k}e3' );
+define( 'NONCE_KEY',        'k{ee7fa0kixIsg2I[h!HKGm?+Loy=(Tq~&vKU7X1ZhR7oyEH&W|RX;PNBA0^{Wx:' );
+define( 'AUTH_SALT',        '8]l1f,&/m_Pi;YQJGnM4+vE^42s;ktCDD-[<g_LybTV9:&pl}:X}sBhG8X58af_e' );
+define( 'SECURE_AUTH_SALT', '/o?}~0SC:ewSr--ind)E[Y#[jqoz{hHv!RM|89:d&y,xmuc6t*GRK~Z3Zc+#mBjD' );
+define( 'LOGGED_IN_SALT',   ')9.79s1KrK6nZCc{10fzI!KGH^>@_?@H-,hlMs%Q^6}w>#$8WP%#Yp*Vi&zR`OwY' );
+define( 'NONCE_SALT',       'l}TPt!BTR;GnJQnI9rSwnCG~c#T[%[`EN4_*QH90>GsJ|i8XY?hHfO?/hmQb+raZ' );
 
 /**#@-*/
 
@@ -101,7 +71,7 @@ define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       '927b15d
  *
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
-$table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'SERVMASK_PREFIX_');
+$table_prefix = 'SERVMASK_PREFIX_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -115,20 +85,16 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'SERVMASK_PREFIX_');
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
-// see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
-	$_SERVER['HTTPS'] = 'on';
-}
-// (we include this by default because reverse proxying is extremely common in container environments)
-
-if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
-	eval($configExtra);
-}
+define( 'FS_METHOD', 'ssh2' );
+define('WP_MEMORY_LIMIT', '512M');
+define('WP_MAX_MEMORY_LIMIT', '768M');
 
 /* That's all, stop editing! Happy publishing. */
 
---
+++ wp-content/uploads/smush/resize-log.php
@@ -0,0 +1,4 @@
+<?php die(); ?>
+[2026-09-14T10:39:40+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
+[2026-09-14T10:39:40+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
+[2026-09-14T10:39:52+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
