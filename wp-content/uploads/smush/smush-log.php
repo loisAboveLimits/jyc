67a7e2c481f4ec9b76f98082a91d1c4a3b2e622f
loisAboveLimits <lois@above-limits.com>
2026-09-14 11:13:25 +0000

    added classic editor and ssh plugins

---
+++ wp-content/uploads/smush/smush-log.php
@@ -0,0 +1,11 @@
+<?php die(); ?>
+[2026-09-14T10:53:51+00:00] Setting transparent meta value
+[2026-09-14T10:53:51+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/26-09-14-135309.png] is a PNG 32-bit.
+[2026-09-14T10:58:48+00:00] Setting transparent meta value
+[2026-09-14T10:58:48+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/01meed.png] is a PNG 32-bit.
+[2026-09-14T10:58:55+00:00] Setting transparent meta value
+[2026-09-14T10:58:55+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/01sea.png] is a PNG 32-bit.
+[2026-09-14T10:59:03+00:00] Setting transparent meta value
+[2026-09-14T10:59:03+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/01awad.png] is a PNG 32-bit.
+[2026-09-14T10:59:14+00:00] Setting transparent meta value
+[2026-09-14T10:59:14+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/01sahil.png] is a PNG 32-bit.
