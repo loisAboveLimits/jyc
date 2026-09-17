4ffbbcffd8edcbe41817e0538ac08bf023336383
loisAboveLimits <lois@above-limits.com>
2026-09-17 07:05:30 +0000

    updates 09172026

--- wp-content/uploads/smush/resize-log.php
+++ wp-content/uploads/smush/resize-log.php
@@ -2,3 +2,6 @@
 [2026-09-14T10:39:40+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
 [2026-09-14T10:39:40+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
 [2026-09-14T10:39:52+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/DSC06142-1.jpg(3816)] does not have the PNG mime-type.
+[2026-09-15T08:25:19+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/02-what-4-1.jpg(3940)] does not have the PNG mime-type.
+[2026-09-15T08:25:19+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/02-what-4-1.jpg(3940)] does not have the PNG mime-type.
+[2026-09-15T08:25:23+00:00] Info: File [/var/www/html/wp-content/uploads/2026/09/02-what-4-1.jpg(3940)] does not have the PNG mime-type.
